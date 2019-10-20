abf05fc1fe24578b8ba850d98c88200e2534f2fb
cegloff <christopher.egloff@gmail.com>
2019-10-19 23:12:55 -0600

    added notifications for X11

--- ads1115/README.md
+++ ads1115/README.md
@@ -9,14 +9,14 @@ Quick script to be used with Raspberry pi to utilize an ADS1115 ADC to check the
 If the voltage is greater than 3.2V then log the voltage once every five minutes.  If the voltage 
 is less than 3.2V use wall to announce to all logged in users that it's low and log the alert.
 
-
-I use a crenjob to run this script every minute and then take action according to the
+I use a cronjob to run this script every minute and then take action according to the
 battery voltage.
 
 
 ## Execution Instructions
 Cron command to run this script every minute, assuming that you have the
-script copied to /scripts/ads1115/get_bat.py
+script copied to /scripts/ads1115/get_bat.py 
+
 */1 * * * * /usr/bin/python3 /scripts/ads1115/get_bat.py >/dev/null 2>&1     
 
 
--- ads1115/get_bat.py
+++ ads1115/get_bat.py
@@ -6,6 +6,7 @@ from time import sleep
 import subprocess
 import syslog
 from datetime import datetime
+import os
 
 #################################################################################################
 # Cron command to run this script every minute                                                  #
@@ -25,9 +26,9 @@ def get_battery():
     # Take three samples and average them
     for i in range(4):
         chan = AnalogIn(ads, ADS.P0)
-        # Measuring with multimeter 2.04 was the correct multiplier for my splitter
+        # Measuring with multimeter 2.043 was the correct multiplier for my splitter
         # this value should be updated for each build
-        current_voltage = chan.voltage * 2.04
+        current_voltage = chan.voltage * 2.043
         avg_bat = avg_bat + current_voltage
     avg_bat = avg_bat/4
 
@@ -36,20 +37,41 @@ def get_battery():
 
 
 def os_announce(message):
-    # Use wall to announce any message passed to the function
+    # Use wall and xmessage to announce any message passed to the function
     # to all logged in users.
     command = "/usr/bin/wall"
     subprocess.call([command, message])
+    os.environ["DISPLAY"] = ':0.0'
+    os.environ["XAUTHORITY"] = '/home/pi/.Xauthority'
+    command = "xmessage"
+    subprocess.call([command, message])
+
+
+def shutdown_system(message):
+    # command = "shutdown"
+    # arg1 = "-h"
+    # arg2 = "-t 120"
+    # subprocess.call([command, arg1, arg2, message])
+    os.system('systemctl poweroff')
 
 
 if __name__ == "__main__":
     current_bat = get_battery()
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    if current_bat < 3.2:
-        # If the battery voltage is critical use wall to send a message to all users
+    if current_bat <= 3.1:
+        # If the battery voltage is critical send a shutdown command and then
+        # use wall to send a message to all users finally log the voltage and
+        # warning to syslog
+        message = 'Voltage is Critial.  System will is shutting down. Current Voltage = ' + \
+            str(current_bat)
+        syslog.syslog("[Critical] Battery Voltage is: %s" % current_bat)
+        os_announce(message)
+        shutdown_system(message)
+    elif current_bat < 3.2 and current_bat > 3.1:
+        # If the battery voltage is low use wall to send a message to all users
         # then log the voltage and warning to syslog
         message = 'BATTERY IS LOW! Current Voltage = ' + str(current_bat)
-        syslog.syslog("[Critical] Battery Voltage is: %s" % current_bat)
+        syslog.syslog("[Error] Battery Voltage is: %s" % current_bat)
         os_announce(message)
     else:
         # If the battery isn't critical log the voltage every 5 minutes
