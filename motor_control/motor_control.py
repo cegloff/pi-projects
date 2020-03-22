e9e7a302a40fec5dcadfe54fee1ede24e47c828d
Chris <christopher.egloff@gmail.com>
2020-03-22 03:42:55 +0000

    Added pwn for motoro enable

--- motor_control/motor_control.py
+++ motor_control/motor_control.py
@@ -1,10 +1,14 @@
 from gpiozero import Motor
 from time import sleep
+from gpiozero import PWMLED
 
-motorR = Motor(forward=4, backward=18)
-motorL = Motor(forward=17, backward=27)
+motorR = Motor(forward=4, backward=18, pwm=False)
+motorL = Motor(forward=17, backward=27, pwm=False)
 
-
+enable1 = PWMLED(12)
+enable2 = PWMLED(13)
+enable1.value = 50 / 100
+enable2.value = 50 / 100
 
 def forward():
 # while True:
